406913a042479168f6ba72b32576654838bf3850
artur <arturbonnevialle@gmail.com>
2026-02-03 16:32:28 -0300

    creat if and return

--- index.js
+++ index.js
@@ -2,28 +2,50 @@
 const cidadeInput = document.getElementById('cidadeInput');
 const buscarBtn = document.getElementById('buscarBtn');
 const cidadeNome = document.getElementById('cidadeNome');
-const tempMin = document.getElementById('tempMin');
+
 const umidade = document.getElementById('umidade');
 const vento = document.getElementById('vento');
+
+const tempMin = document.getElementById('tempMin');
 const tempMax= document.getElementById('tempMax');
+
+const clima = document.getElementById('clima');
 const tempAtual = document.getElementById('tempAtual');
 
 // Evento de clique no botão Buscar
 buscarBtn.addEventListener('click', () => {
 
 // Colocar texto de cidade digitada
-cidadeInput.addEventListener
+const cidadeDigitada = cidadeInput.value.trim();
 
 // Mudar texto da  cidade
-cidadeNome.innerText = 'São Paulo';
+
+if (cidadeDigitada === '') {
+    cidadeNome.innerText = 'Digite o nome de uma cidade';
+    return;
+}  
+
+if (cidadeDigitada.toLowerCase() === 'londres') {
+    cidadeNome.innerText = 'Londres';
+    tempMin.innerText = 7;
+    tempMax.innerText = 15;
+    umidade.innerText = 81;
+    vento.innerText = 10;
+    clima.innerText = 'Nublado';
+    tempAtual.innerText = '11 °C';
+    return;
+}  
 
 // Mudar status da tela
-cidadeNome.innerText = 'São Paulo';
+cidadeNome.innerText = cidadeDigitada;
+
 tempMin.innerText = 16;
 tempMax.innerText = 26;
+
 umidade.innerText = 78;
 vento.innerText = 15;
+
 clima.innerText = 'Ensolarado';
-tempAtual.innerText = 22 + '°C';
+tempAtual.innerText = '22 °C';
 
 });
